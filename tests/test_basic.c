6253e6d093a294c96cf05f0e7e0761213a50c67f
Rhuanr3is <rhuan.reis@discente.ufma.br>
2026-07-08 18:48:29 -0300

    retest

--- tests/test_basic.c
+++ tests/test_basic.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
 void bubbleSort(int v[], int n);
-
 void selection(int v[], int n);
-
 void insertion(int v[], int n);
-
 void mergeSort(int v[], int ini, int fim);
-
 void quickSort(int v[], int low, int high);
-
 void heapSort(int v[], int n);
 
 int verificarOrdenado(int v[], int n){
-    for(int i=0;i<n-1;i++){
+    for(int i = 0; i < n - 1; i++){
         if(v[i] > v[i+1]){
             return 0;
         }
@@ -21,55 +16,77 @@ int verificarOrdenado(int v[], int n){
     return 1;
 }
 
+void gerarFibonacciInvertido(int v[], int n) {
+    int a = 0, b = 1, proximo;
+    for (int i = 0; i < n; i++) {
+        v[n - 1 - i] = a;
+        proximo = a + b;
+        a = b;
+        b = proximo;
+    }
+}
+
 void testarBubble(){
-    int v[] = {19,18,171,14,11};
-    bubbleSort(v,5);
-    if(verificarOrdenado(v,5))
+    int n = 40;
+    int v[40];
+    gerarFibonacciInvertido(v, n);
+    bubbleSort(v, n);
+    if(verificarOrdenado(v, n))
         printf("Bubble executado com exito\n");
     else
         printf("Bubble com falha\n");
 }
 
 void testarSelection(){
-    int v[] = {9,2,6,1,5};
-    selection(v,5);
-    if(verificarOrdenado(v,5))
+    int n = 40;
+    int v[40];
+    gerarFibonacciInvertido(v, n);
+    selection(v, n);
+    if(verificarOrdenado(v, n))
         printf("Selection executado com exito\n");
     else
         printf("Selection com falha\n");
 }
 
 void testarInsertion(){
-    int v[] = {7,3,8,1,2};
-    insertion(v,5);
-    if(verificarOrdenado(v,5))
+    int n = 40;
+    int v[40];
+    gerarFibonacciInvertido(v, n);
+    insertion(v, n);
+    if(verificarOrdenado(v, n))
         printf("Insertion executado com exito\n");
     else
         printf("Insertion com falha\n");
 }
 
 void testarMerge(){
-    int v[] = {10,7,4,8,2};
-    mergeSort(v, 0, 4);
-    if(verificarOrdenado(v,5))
+    int n = 40;
+    int v[40];
+    gerarFibonacciInvertido(v, n);
+    mergeSort(v, 0, n - 1);
+    if(verificarOrdenado(v, n))
         printf("Merge executado com exito\n");
     else
         printf("Merge com falha\n");
 }
 
 void testarQuick(){
-    int v[] = {6,1,9,3,2};
-    quickSort(v, 0, 4);
-    if(verificarOrdenado(v,5))
+    int n = 40;
+    int v[40];
+    gerarFibonacciInvertido(v, n);
+    quickSort(v, 0, n - 1);
+    if(verificarOrdenado(v, n))
         printf("Quick executado com exito\n");
     else
         printf("Quick com falha\n");
 }
 
 void testarHeap(){
-    int v[] = {12,4,8,2,1};
-    heapSort(v,5);
-    if(verificarOrdenado(v,5))
+    int n = 40;
+    int v[40];
+    gerarFibonacciInvertido(v, n);
+    heapSort(v, n);
+    if(verificarOrdenado(v, n))
         printf("Heap executado com exito\n");
     else
         printf("Heap com falha\n");
